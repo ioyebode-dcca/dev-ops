86f2674f55d7c917bd99b2ef38b3ca5706c76ee2
Israel Oyebode <38587565+oyebode23@users.noreply.github.com>
2023-03-25 12:04:58 -0400

    Update main.tf

--- Terraform-AWS-Services-Creation/terraform-state-s3-bucket/main.tf
+++ Terraform-AWS-Services-Creation/terraform-state-s3-bucket/main.tf
@@ -1,18 +1,23 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-devopsthehardway"
-  versioning {
-    enabled = true
-  }
+  bucket = "terraform-state-fwit"
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_versioning" "versioning_fwit" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_fwit" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
     }
   }
 }
